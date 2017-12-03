98fe0d1a3befa31c7c6c83c0c90ffa67418f5ed7
akiet00 <akiet00@yahoo.com>
2017-12-02 18:49:00 -0600

    update

--- Electron App/plots.js
+++ Electron App/plots.js
@@ -1,12 +1,65 @@
-/*
-(function(){
-  var data = [
-  {
-    x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-    y: [1, 3, 6],
-    type: 'scatter'
-  }
-];
+$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
 
-  Plotly.newPlot('alt-time', data);
-})();
+    var alt_chart = Highcharts.chart('alt-time', {
+        chart: {
+            //zoomType: 'x',
+            borderColor: 'rgba(0,0,0',
+            zoomType: 'x'
+        },
+        title: {
+          text: "Testing Plots",
+          style:{
+            color: "#fff"
+          }
+        },
+        xAxis: {
+            type: 'datetime',
+            style:{
+              color: '#fff'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Exchange rate'
+            },
+            style:{
+              color: '#fff'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series: [{
+            type: 'area',
+            name: 'USD to EUR',
+            data: data
+        }]
+    });
+});
