bba48cac455d3f6bd741e8dcd541bf6381c37c55
akiet00 <akiet00@yahoo.com>
2017-12-03 01:20:31 -0600

    live data update with random function

--- Electron App/javascripts/plots.js
+++ Electron App/javascripts/plots.js
@@ -6,6 +6,84 @@ we should figure out how to plot data using serial data stream.
 */
 
 var myvar = ['alt-time','vel-time','pressure-time','temperature-time'];
+
+Highcharts.setOptions({
+    global: {
+        useUTC: false
+    }
+});
+
+for(i=0; i< myvar.length; i++){
+Highcharts.chart(myvar[i], {
+    chart: {
+        type: 'spline',
+        animation: Highcharts.svg, // don't animate in old IE
+        marginRight: 10,
+        events: {
+            load: function () {
+
+                // set up the updating of the chart each second
+                var series = this.series[0];
+                setInterval(function () {
+                    var x = (new Date()).getTime(), // current time
+                        y = Math.random();
+                    series.addPoint([x, y], true, true);
+                }, 1000);
+            }
+        }
+    },
+    title: {
+        text:null
+    },
+    xAxis: {
+        type: 'datetime',
+        tickPixelInterval: 150
+    },
+    yAxis: {
+        title: {
+            text: 'Random Value'
+        },
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }]
+    },
+    tooltip: {
+        formatter: function () {
+            return '<b>' + this.series.name + '</b><br/>' +
+                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                Highcharts.numberFormat(this.y, 2);
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    exporting: {
+        enabled: false
+    },
+    series: [{
+        name: 'Random data',
+        data: (function () {
+            // generate an array of random data
+            var data = [],
+                time = (new Date()).getTime(),
+                i;
+
+            for (i = -19; i <= 0; i += 1) {
+                data.push({
+                    x: time + i * 1000,
+                    y: Math.random()
+                });
+            }
+            return data;
+        }())
+    }]
+});
+}
+
+/*
+Get data from JSON
 $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
 for(i=0; i< myvar.length; i++){
     var alt_chart = Highcharts.chart(myvar[i], {
@@ -72,3 +150,4 @@ for(i=0; i< myvar.length; i++){
     });
   }
 });
+*/
