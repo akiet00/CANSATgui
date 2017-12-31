a22f90146be5749f6f20ebca484536e94862cb10
akiet00 <akiet00@yahoo.com>
2017-12-31 15:07:36 -0600

    new file

--- Electron App/javascripts/serial.js
+++ Electron App/javascripts/serial.js
@@ -1,7 +1,36 @@
+
+/*
+*/
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 
-var serialPort = new SerialPort("/dev/cu.usbmodem14131", {
-  baudrate: 9600,
+/* How do we allow options to choose the serial port?
+* We can choose the serial port from our drop-down menu in GUI and
+* set myPortName to what we select. The same can be done for
+* myBaudrate as well.
+* But for now, we can set some random port for development
+*/
+myPortName = "COM4";
+myBaudrate = 9600;
+
+var serialPort = new SerialPort( myPortName, {
+  baudrate: myBaudrate,
   parser: serialport.parsers.readline("\n")
 });
+
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    console.log(data); //Dump serial data into console for monitoring
+    writeToCSV(data);
+  });
+});
+
+
+// Function to write data to CSV files
+// Input: An array of data type double
+// Return: None
+// Output: Write to CSV file
+function writeToCSV(){
+
+}
